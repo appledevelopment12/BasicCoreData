3aa6c751ad8dd8adc66cfcf2b4de59516afb577f
Rohit <appledevelopment12@gmail.com>
2025-07-05 11:10:37 +0530

    Initial Commit

---
+++ BasicCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicCoreData
+//
+//  Created by Rohit on 05/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
